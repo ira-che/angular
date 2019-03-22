b2ca1d28a48b6f86ab2e1e2f0778534f0a9e0b32
Diana <sundgirl19@gmail.com>
2019-03-22 13:25:27 +0200

    Add List with Accordion

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,26 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PageComponent } from './page/page.component';
-
-import { FilterComponent } from './page/filter/filter.component';
-import { DropdownFilterComponent } from './page/filter/dropdown-filter/dropdown-filter.component';
+import { ListComponent } from './page/list/list.component';
+import { AccordionComponent } from './page/list/accordion/accordion.component';
+import { AccordionModule } from 'ngx-bootstrap/accordion';
 
 @NgModule({
   declarations: [
     AppComponent,
     PageComponent,
-    FilterComponent,
-    DropdownFilterComponent
+    ListComponent,
+    AccordionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BsDropdownModule.forRoot()
+    AppRoutingModule, 
+    AccordionModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
