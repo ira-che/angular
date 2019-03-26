4de91c443c5d6cfccf0cbe76a45407ad05e4ec97
Oleksii <lebediew.oleksii@gmail.com>
2019-03-26 23:29:20 +0200

    navbar_component_header fixed all and add response

--- client/src/app/page/navbar/navbar.component.ts
+++ client/src/app/page/navbar/navbar.component.ts
@@ -8,15 +8,21 @@ import { Component, OnInit, Input } from '@angular/core';
 export class NavbarComponent implements OnInit {
   @Input() menuBurger:any;
   @Input() menuRight:any;
+  name: string = "Name";
+  surname: string = "Surname";
+  avatar: string = "assets/img/navbar-symbol-desk.png";
   constructor() { }
 
   ngOnInit() {
   }
   changeCurrent(index, links) {
-      event.preventDefault();
-      links.forEach(item => {
-        item.isCurrent = false
-      })
-      links[index].isCurrent = true;
+    event.preventDefault();
+    links.forEach(item => {
+      item.isCurrent = false
+    })
+    links[index].isCurrent = true;
+    if(links[index].title === "Log Out") {
+      links[index].isCurrent = false
     }
+  }
 }
